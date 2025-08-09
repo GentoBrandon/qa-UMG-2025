00d92c051cea92663940a03b106ebff7f7a4787d
GentoBrandon <brandonailon19@gmail.com>
2025-08-09 15:32:59 -0600

    chore: add ci for test in week-5

--- week-5/src/stack.ts
+++ week-5/src/stack.ts
@@ -7,6 +7,7 @@ export function balancedBrackets(str) {
         '{': '}',
         '[': ']'
     };
+    console.log(str);
     let lastOpeningBracket: string;
     for (const char of str){
         if (openingBrackets.includes(char)){
